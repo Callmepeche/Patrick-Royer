4337d8fe69b6595f58a74266a06ad710c595d71e
Callmepeche <pcr-97@hotmail.com>
2016-08-30 00:31:37 -0300

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -9,8 +9,10 @@ public Triangle(double a, double b)
 }
 
 public void computeC()
+//this bracket is a symbol error
 {
-      (double)c = (a*0.5)*b;
+      double c = (double)(a*0.5)*b;
+      return c;
 }
 
 public void setA (double a)
@@ -33,6 +35,7 @@ public double getB ()
       return b;
 }
 
+//this tostring is also a symbol error and so is the getA, getB and c
 public string toString()
 {
   return "Side A is equal to: " + getA + "Side B is equal to: " + getB 
